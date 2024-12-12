50da34fbe5a6b65133c11fab1e97089f161b5681
Haidyasser <haidyasser3@gmail.com>
2024-12-12 23:53:20 +0200

    return to previous version

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,15 +2,14 @@
 """Prime Game"""
 
 
-def sieve_of_eratosthenes(n):
+def seive(n):
     """Sieve of Eratosthenes"""
-    primes = [True for _ in range(n + 1)]
-    primes[0] = primes[1] = False  # 0 and 1 are not prime
-    for i in range(2, int(n ** 0.5) + 1):
-        if primes[i]:
-            for j in range(i * i, n + 1, i):
-                primes[j] = False
-    return primes
+    prime = [True for i in range(n+1)]
+    for i in range(2, n+1):
+        if prime[i]:
+            for j in range(i*i, n+1, i):
+                prime[j] = False
+    return prime
 
 
 def isWinner(x, nums):
